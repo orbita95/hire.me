3956f2994b110ccb200486c1b1811827c20dcb2a
Samuel Pereira <Samuel.Pereira@br.ey.com>
2020-06-05 21:45:41 -0300

    clientewebapp correto

---
+++ EncurtadorAspNet/WebAppEncurtadorUrlClient/WebApplication1/WebApplication1/Forms/frmEncurtadorUrl.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="frmEncurtadorUrl.aspx.cs" Inherits="WebApplication1.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <style>
+
+        .content-box {
+            margin-top: 20px;
+            margin-left:2em;
+        }
+
+    </style>
+
+    <div class="content-box">
+        <div class="row">
+            <div class="col-md-4">
+                <label>URL </label>
+            </div>
+            <div class="col-md-4">
+                <asp:TextBox runat="server" ID="urlPadrao_tb" TextMode="Url"></asp:TextBox>
+            </div>
+        
+        </div>
+        <div class="row">
+        <div class="col-md-4">
+            <label>Alias (Opcional) </label>
+        </div>
+        <div class="col-md-4">
+            <asp:TextBox runat="server" ID="alias_tb"  ></asp:TextBox>
+        </div>
+    </div>
+        <div class="row">
+            <div class="col-md-4">
+                <asp:Button runat="server" OnClick="Unnamed_Click1" Text="Encurtar" />
+            </div>
+            <div class="col-md-4">
+                <asp:Button runat="server" Text="Buscar URL" OnClick="Unnamed_Click" />
+            </div>
+            
+        </div>
+
+        <hr />
+
+        <div class="row">
+            <div class="col-md-4">
+                <asp:Label runat="server" ID="retorno_lbl"></asp:Label>
+            </div>
+        </div>
+    </div>
+
+    
+</asp:Content>
---
+++ EncurtadorAspNet/WebAppEncurtadorUrlClient/WebApplication1/WebApplication1/Forms/frmEncurtadorUrl.aspx.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+
+
+
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        protected void Unnamed_Click1(object sender, EventArgs e)
+        {
+            var response = PostUrlEncurtada(urlPadrao_tb.Text, alias_tb.Text);
+            retorno_lbl.Text = response.ToString();
+        }
+
+        protected void Unnamed_Click(object sender, EventArgs e)
+        {
+            var response = GetUrlEncurtada(alias_tb.Text);
+
+            retorno_lbl.Text = response.ToString();
+        }
+
+        private object PostUrlEncurtada(string url, string alias)
+        {
+            var urlPost = new StringBuilder("https://localhost:44368/api/URLs?");
+            
+            if (String.IsNullOrEmpty(alias))
+            {
+                urlPost.AppendFormat("url={0}", url);
+            }
+            else
+            {
+                urlPost.AppendFormat("url={0}&alias={1}", url, alias);
+            }
+
+            return makePostRequest(urlPost.ToString());
+
+
+        }
+
+        private object GetUrlEncurtada(string alias)
+        {
+            var urlPost = new StringBuilder("https://localhost:44368/api/URLs?");
+            urlPost.AppendFormat("alias={0}",  alias);
+
+            return makeGetRequest( urlPost.ToString());
+
+
+        }
+
+        private object makeGetRequest(string urlGet) {
+            var requisicaoWeb = (HttpWebRequest)WebRequest.Create(urlGet);
+            requisicaoWeb.AutomaticDecompression = DecompressionMethods.GZip;
+
+
+            requisicaoWeb.CookieContainer = new CookieContainer();
+            requisicaoWeb.Method = "GET";
+            
+
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+            using (var resposta = (HttpWebResponse)requisicaoWeb.GetResponse())
+            {
+                var streamDados = resposta.GetResponseStream();
+                StreamReader reader = new StreamReader(streamDados);
+                object objResponse = reader.ReadToEnd();
+                return objResponse;
+
+
+
+            }
+        }
+
+
+        private object makePostRequest( string urlPost)
+        {
+            var requisicaoWeb = (HttpWebRequest)WebRequest.Create(urlPost);
+            requisicaoWeb.AutomaticDecompression = DecompressionMethods.GZip;
+
+
+            requisicaoWeb.CookieContainer = new CookieContainer();
+            requisicaoWeb.Method = "POST";
+            requisicaoWeb.ContentLength = 0;
+
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+            using (var resposta = (HttpWebResponse)requisicaoWeb.GetResponse())
+            {
+                var streamDados = resposta.GetResponseStream();
+                StreamReader reader = new StreamReader(streamDados);
+                object objResponse = reader.ReadToEnd();
+                return objResponse;
+
+
+
+            }
+        }
+
+
+    }
+
+
+    internal class RetornoUrl
+    {
+        public string Description { get; set; }
+        public string UrlEncurtada { get; set; }
+        public string UrlOriginal { get; set; }
+    }
+
+}
